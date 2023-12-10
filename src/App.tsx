a5c2351c33bdad5d0c2d6cf1a4f807e7deca8fdc
madeel42 <madeelakhtar204@gmail.com>
2023-12-11 01:12:37 +0500

    logs are added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css';
-import { ApolloProvider } from '@apollo/client';
-import { client } from './Config/index';
-import { LaunchesContainer } from './components/Launches/index'
+import "./App.css";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./Config/index";
+import { LaunchesContainer } from "./components/Launches/index";
 
 function App() {
-  return <ApolloProvider client={client}>
-    <LaunchesContainer />
-  </ApolloProvider>
+  console.log("test");
+  return (
+    <ApolloProvider client={client}>
+      <LaunchesContainer />
+    </ApolloProvider>
+  );
 }
 
 export default App;
